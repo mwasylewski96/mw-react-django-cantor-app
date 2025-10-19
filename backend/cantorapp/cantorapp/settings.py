5b14e8f2ef13e21330f15d5ae5e442cb8ca0c248
Marcin Wasylewski <m.wasylewski96@gmail.com>
2025-10-19 18:17:10 +0200

    correction of allow headers

--- backend/cantorapp/cantorapp/settings.py
+++ backend/cantorapp/cantorapp/settings.py
@@ -108,4 +108,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "x-currency-token",
+]
